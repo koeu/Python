3180846062339fdca80e32eef7df995995df8eb7
Eunbyeol Ko <geb5161@naver.com>
2021-04-23 19:07:25 +0900

    create animalfarm_module.py

---
+++ animalfarm_module.py
@@ -0,0 +1,27 @@
+'''
+양 - “메에에”
+닭 - “꼬끼오”
+개 - “크르릉”
+
+animal.py 모듈 안에 동물들의 울음소리를 담은 리스트 animals 를 보고 각 동물이 몇 마리가 있는지 세어보려고 합니다.
+
+'''
+
+# 동물 변수(sheep, chicken, dog)를 선언하고, 
+# animal 모듈의 animals 리스트 안의 동물 마리수를 동물 변수에 넣어봅시다.
+import animal
+
+ss = animal.animals
+print(ss)
+
+def cry(ss, cc):
+    count = 0
+    for i in ss:
+        if i == cc:
+            count = count + 1
+    return count
+
+# 변수 sheep, chicken, dog를 선언하고, animal 모듈의 animals 리스트 안의 동물 마리수를 각 변수에 저장
+sheep = cry(ss, "메에에")
+chicken = cry(ss, "꼬끼오")
+dog = cry(ss, "크르릉")
